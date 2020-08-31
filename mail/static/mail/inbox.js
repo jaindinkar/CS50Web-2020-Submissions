a1ed5338c483e45b17be86e20de5ed82fdf19fa7
Dinkar Jain <jain.dinkar675@gmail.com>
2020-09-01 00:03:47 +0530

    Feature 5 complete

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -4,28 +4,52 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
     document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-    document.querySelector('#compose').addEventListener('click', compose_email);
+    document.querySelector('#compose').addEventListener('click', () => compose_email());
 
     // By default, load the inbox
     load_mailbox('inbox');
 });
 
-function compose_email() {
+function compose_email(emailData) {
 
     // Show compose view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'block';
     document.querySelector('#email-view').style.display = 'none';
 
-    // Clear out composition fields
-    // I personally don't think that it is necessary. I tried commenting and running the code
-    // the fields clear themselves automatically.
+    // Empty form fields to remove stale content.
     document.querySelector('#compose-recipients').value = '';
     document.querySelector('#compose-subject').value = '';
     document.querySelector('#compose-body').value = '';
 
-    // Add your send mail code here
 
+    // If data is given in the function's argument, preload the form values with appropriate data.
+    // Argument is passed when this function is called for replying a message.
+    if(emailData !== undefined) {
+
+        // Enumerate form fields with given data.
+        document.querySelector('#compose-recipients').value = emailData.sender;
+        
+        // Modified subject field
+        // Append "Re: " string to subject line.
+        var pos = emailData.subject.search("Re:");
+        if(pos < 0) {
+            var subjectText = "Re:".concat(" ", emailData.subject);
+        }
+        else {
+            var subjectText = emailData.subject;
+        }
+        document.querySelector('#compose-subject').value = subjectText;
+        
+        // Modified Body field.
+        document.querySelector('#compose-body').value = `On ${emailData.timestamp} ${emailData.sender} wrote: \n ${emailData.body} \n ---------------------------------------- \n\n`;
+        
+        // Use following line for checking the properties of the input object.
+        // `Type: ${typeof emailData} Length: ${Object.keys(emailData).length} Constructor: ${emailData.constructor}`       
+    }
+
+
+    // Following code will execute when the form is submitted.
     // Get the form data and send it using API when user submits the form.
     document.querySelector('form').onsubmit = function() {
 
@@ -60,29 +84,6 @@ function compose_email() {
             console.log('Error', error);
         });
 
-/*
-
-        // // Approach-1 Checking if the mail is properly sent (Not Working)
-        // if(result.message){
-        //     // Load the sent page if message is delivered successfully.
-        //     load_mailbox('sent');
-        // }
-
-        // // Approach-2 Checking if the mail is properly sent (Not Working)
-        // var obj = JSON.parse(result, function(key, value) {
-        //     if (key == "message") {
-        //         return load_mailbox('sent');
-        //     }
-        // })
-
-        // // Approach-3 Checking if the mail is properly sent (Not Working)
-        // if(result['message'] !== undefined){
-        //     // Load the sent page if message is delivered successfully.
-        //     load_mailbox('sent');
-        // }
-
-        // Above approaches do not work as the result variable is not in the domain.
-*/
         //Prevent default submission (Important!! Otherwise the message won't log on your chrome.)
         return false;
 
@@ -92,7 +93,7 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
   
-    // Show the mailbox and hide other views
+    // Show the mailbox and hide other views.
     document.querySelector('#emails-view').style.display = 'block';
     document.querySelector('#compose-view').style.display = 'none';
     document.querySelector('#email-view').style.display = 'none';
@@ -100,7 +101,7 @@ function load_mailbox(mailbox) {
     // Clear the mailbox area of stale content.
     document.querySelector('#emails-view').innerHTML = "";
 
-    // Show the mailbox name
+    // Show the mailbox name.
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
 
@@ -113,24 +114,11 @@ function load_mailbox(mailbox) {
         //Print emails
         console.log(emails);
 
-/*
         // Write code for displaying your mails in mailbox.
 
         // Each divison shoud display: sender's name, subject line and timestamp.
         // Unread = white background , Read = gray background.
         
-        // JSON Data would be indexed like this. 
-        // 0: {
-        // "id": 95,
-        // "sender": "baz@example.com",
-        // "recipients": ["bar@example.com"],
-        // "subject": "Meeting Tomorrow",
-        // "body": "What time are we meeting?",
-        // "timestamp": "Jan 1 2020, 12:00 AM",
-        // "read": true,
-        // "archived": false
-        // }
-*/
         // Approach-2 Iterating through each mail object using forEach method.
         emails.forEach(function(item, mail_index, email_obj) {
 
@@ -181,58 +169,6 @@ function load_mailbox(mailbox) {
             
         })
 
-/*
-
-        // Iterating through each mail object using mail index.
-        // https://stackoverflow.com/questions/19323699/iterating-through-json-object-javascript
-        for(var mail_index in emails) {
-
-            // Extracting the required properties from each mail object
-            if(emails.hasOwnProperty(mail_index)) {
-                var email_sender = emails[mail_index].sender;
-                var email_subject = emails[mail_index].subject;
-                var email_timestamp = emails[mail_index].timestamp;
-                var email_isRead = emails[mail_index].read;
-                var email_id = emails[mail_index].id;
-
-                // Creating a divison for each mail object.
-                const mail_sub_div = document.createElement('div');
-
-                // Adding the HTML content in the division.
-                mail_sub_div.innerHTML = `
-
-                    <h2>From: ${email_sender}</h2> 
-                    <h4>Subject: ${email_subject}</h4>
-                    <p>Time: ${email_timestamp}</p>
-
-                `;
-
-                // Setting the background color of the division using the read property of mail object.
-                if(email_isRead){
-                    // https://www.w3schools.com/JSREF/prop_style_backgroundcolor.asp
-                    // mail_sub_div.style.background = "color image repeat attachment position size origin clip|initial|inherit";
-                    mail_sub_div.style.background = "#ededed";
-                }
-                else {
-                    mail_sub_div.style.background = "#ffffff";
-                }
-
-                // Setting a class name for this division for further styling using CSS.
-                mail_sub_div.className = "mail-sub-div-style";
-
-                // Setting an eventListener to this element, when clicked.
-                // This function does not correctly work with for loop but works good with forEach function.
-                mail_sub_div.addEventListener('click', function() {
-                    // When this element it clicked following code is executed.
-                    console.log(`Mail with id ${email_id} is clicked.`)
-
-                })
-
-                // Appending the divison in the element with ID emails-view.
-                document.querySelector('#emails-view').append(mail_sub_div);
-            }
-        }
-*/
     });
 }
 
@@ -248,14 +184,6 @@ function load_mail(mail_id, mailbox) {
     // Clearing the previous stale content.
     document.querySelector('#email-view').innerHTML = "";
 
-    // Marking the clicked email as read. PUT request through API.
-    fetch(`/emails/${mail_id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            read: true
-        })
-    })
-
     // Fetch the requested mail data by querying the API
     fetch(`/emails/${mail_id}`)
     .then(response => response.json())
@@ -270,8 +198,19 @@ function load_mail(mail_id, mailbox) {
         var email_subject = email.subject;
         var email_timestamp = email.timestamp;
         var email_body = email.body;
+        var email_isRead = email.read;
 
-
+        // If email is not read, mark it read.
+        if(!email_isRead){
+            // Marking the clicked email as read. PUT request through API.
+            fetch(`/emails/${mail_id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    read: true
+                })
+            })
+        }
+        
         // Creating a divison for placing content of requested mail.
         const mail_view_div = document.createElement('div');
 
@@ -293,15 +232,15 @@ function load_mail(mail_id, mailbox) {
         // Appending the created division to email-view division. (Look inbox.html for more.)
         document.querySelector('#email-view').append(mail_view_div);
 
-        //  Archive option for inbox mails.
+        // Special features for inbox mails.
         if(mailbox === "inbox") {
-            // Creating a button for archive.
+            // Feature I - Creating a button for archive.
             const archiveButton = document.createElement('BUTTON');
             archiveButton.innerHTML = "Archive";
             archiveButton.addEventListener('click', function() {
 
                 // Debug log for button press event.
-                console.log(`Archive button is pressed for ${mail_id}`);
+                console.log(`Archive button is pressed for mail-id: ${mail_id}`);
 
                 // Marking email as archived. PUT request through API.
                 fetch(`/emails/${mail_id}`, {
@@ -324,17 +263,55 @@ function load_mail(mail_id, mailbox) {
 
             // Appending the archive button to email-view division.
             document.querySelector('#email-view').append(archiveButton);
+
+
+            // Feature II - Creating a button for reply.
+            const replyButton = document.createElement('BUTTON');
+            replyButton.innerHTML = "Reply";
+
+
+            // M1
+            // Using .onclick
+            replyButton.onclick = function() {
+
+                // Debug log for button press event.
+                console.log(`Reply button is pressed for mail-id: ${mail_id}`);
+
+                // Calling the compose-email function for reply.
+                compose_email(email);
+
+            };
+
+            // M2
+            // Using eventListener('click')
+            // replyButton.addEventListener('click', function() {
+
+            //     // Debug log for button press event.
+            //     console.log(`Reply button is pressed for mail-id: ${mail_id}`);
+
+            //     // Calling the compose-email function for reply.
+            //     compose_email(email);
+            // });
+
+
+            // Adding class name to button, auto style by bootstrap.
+            replyButton.className = "btn btn-sm btn-outline-primary";
+
+            // Appending the reply button to email-view division.
+            document.querySelector('#email-view').append(replyButton);
+
         }
 
-        //  Unarchive option for archived mails.
+
+        // Special features for archived mails.
         if(mailbox === "archive") {
-            // Creating a button to unarchive.
+            // Feature I - Creating a button to unarchive.
             const unarchiveButton = document.createElement('BUTTON');
             unarchiveButton.innerHTML = "Unarchive";
             unarchiveButton.addEventListener('click', function() {
 
                 // Debug log for button press event.
-                console.log(`Archive button is pressed for ${mail_id}`);
+                console.log(`Archive button is pressed for mail-id: ${mail_id}`);
 
                 // Marking email as unarchived. PUT request through API.
                 fetch(`/emails/${mail_id}`, {
