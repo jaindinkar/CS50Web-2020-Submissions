98453b338e0966323509f3a27aa9eadefedcea1e
Dinkar Jain <jain.dinkar675@gmail.com>
2020-09-23 05:35:28 +0530

    Project Updated

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# You will also need to add additional models to this file to represent 
+# details about posts, likes, and followers. Remember that each time you 
+# change anything in network/models.py, you’ll need to first run python 
+# manage.py makemigrations and then python manage.py migrate to migrate 
+# those changes to your database.
+
