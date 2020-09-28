1b1da97f0f14b48034b36b0c8be961dd74a49161
Dinkar Jain <jain.dinkar675@gmail.com>
2020-09-29 01:25:44 +0530

    Added paginator module

--- network/views.py
+++ network/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 from .models import User, Post
 from .forms import NewPostForm
 from django.contrib.auth.decorators import login_required
-
+from django.core.paginator import Paginator
 
 def index(request):
 
