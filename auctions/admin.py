da3b8156ffe945ad9ffaef3d24cc72846dccbf22
Dinkar Jain <jain.dinkar675@gmail.com>
2021-05-23 11:57:29 +0530

    Moved project 2 files to a subdir

--- auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Listing, Bid, Comment
-
-
-# Register your models here.
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
